73013cf3fdb4c4f334fad6f8fac1d152a4b1d004
Kelvin F. Baez Rogriguez <kelvin12378945610@gmail.com>
2022-08-25 20:47:53 -0400

    [add] currency converter project

--- src/js/scrollReveal.js
+++ src/js/scrollReveal.js
@@ -260,6 +260,17 @@ ScrollReveal().reveal(".projects-container__card:nth-child(7)", {
     y: 20,
   },
 });
+ScrollReveal().reveal(".projects-container__card:nth-child(8)", {
+  reset: true,
+  duration: 2000,
+  delay: 800,
+  scale: 0.8,
+
+  rotate: {
+    x: -40,
+    y: 20,
+  },
+});
 
 // ScrollReveal contact
 ScrollReveal().reveal(".footer-contact-item-1", {
